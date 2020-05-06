caea271aab9ce399e7a4a987077d87d3630061bf
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2020-05-06 16:48:25 -0400

    Favicon added

--- web/includes/header.php
+++ web/includes/header.php
@@ -1,10 +1,9 @@
 <?php session_start(); ?>
 <?php include "admin/functions.php"; ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-
+  <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,13 +11,9 @@
     <meta name="author" content="">
 
     <title>Blog Home - Start Bootstrap Template</title>
-
-    <!-- Bootstrap Core CSS -->
+    <link rel="shortcut icon" href="/images/favicon.ico" />
     <link href="/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
     <link href="/css/blog-home.css" rel="stylesheet">
-
     <link href="/css/styles.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -27,9 +22,5 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
-
-
-</head>
-
-<body>
+  </head>
+  <body>
