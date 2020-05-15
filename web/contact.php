f74c7cbc9900543e41f602813b8bed50fceb7929
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2020-05-15 18:40:09 -0400

    Updated Sendgrid credentials

--- web/contact.php
+++ web/contact.php
@@ -9,7 +9,7 @@
     $email = new \SendGrid\Mail\Mail(); 
     $email->setFrom("test@example.com", "Example User");
     $email->setSubject("Sending with SendGrid is Fun");
-    $email->addTo("test@example.com", "Example User");
+    $email->addTo("kelley.andrew.t@gmail.com", "Example User");
     $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
     $email->addContent(
         "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
