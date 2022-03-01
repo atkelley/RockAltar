bea2d28c5cefd63f112c90a58fbd94ecd759291a
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2022-03-01 14:38:12 -0500

    Updated footer

--- web/includes/footer.php
+++ web/includes/footer.php
@@ -3,7 +3,7 @@
     <footer>
       <div class="row">
         <div class="col-lg-12 text-center">
-          <p class="footer-left">Copyright &copy; 2020 RockAltar </p>|<p class="footer-right"><a href="https://ak-47.biz" target="_blank">AK-47</a></p>
+          <p class="footer-left">Copyright &copy; 2022 RockAltar </p>|<p class="footer-right"><a href="https://ak-47.biz" target="_blank">AK-47</a></p>
         </div>
       </div>
     </footer>
