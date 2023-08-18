849757db549435b8065121b40a7f2414ae27c1a0
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2023-08-18 11:15:32 -0400

    Created login check around commenting logic

--- web/article.php
+++ web/article.php
@@ -63,27 +63,29 @@
         }
       ?> 
 
-      <div class="well">
-        <h4>Leave a Comment:</h4>
-        <form action="" method="post" role="form">
-          <div class="form-group">
-            <label for="Author">Author</label>
-            <input type="text" name="comment_author" class="form-control" name="comment_author">
-          </div>
+      <?php if(logged_in()){ ?>
+          <div class="well">
+            <h4>Leave a Comment:</h4>
+            <form action="" method="post" role="form">
+              <div class="form-group">
+                <label for="Author">Author</label>
+                <input type="text" name="comment_author" class="form-control" name="comment_author">
+              </div>
 
-          <div class="form-group">
-            <label for="Author">Email</label>
-            <input type="email" name="comment_email" class="form-control" name="comment_email">
-          </div>
+              <div class="form-group">
+                <label for="Author">Email</label>
+                <input type="email" name="comment_email" class="form-control" name="comment_email">
+              </div>
 
-          <div class="form-group">
-            <label for="comment">Your Comment</label>
-            <textarea name="comment_content" class="form-control" rows="3"></textarea>
+              <div class="form-group">
+                <label for="comment">Your Comment</label>
+                <textarea name="comment_content" class="form-control" rows="3"></textarea>
+              </div>
+              <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
+            </form>
           </div>
-          <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
-        </form>
-      </div>
-      <hr>
+          <hr>
+        <?php } ?>
 
       <div id="comments">
         <?php 
