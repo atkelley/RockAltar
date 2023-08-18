bd8a7477373ae15cd0cd22d2121ad82027ed3190
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2023-08-18 11:24:56 -0400

    Added placeholder text to commenting logic

--- web/article.php
+++ web/article.php
@@ -69,12 +69,12 @@
             <form action="" method="post" role="form">
               <div class="form-group">
                 <label for="Author">Author</label>
-                <input type="text" name="comment_author" class="form-control" name="comment_author">
+                <input type="text" name="comment_author" class="form-control" name="comment_author" placeholder="<?php echo $_SESSION['username'] ?>">
               </div>
 
               <div class="form-group">
                 <label for="Author">Email</label>
-                <input type="email" name="comment_email" class="form-control" name="comment_email">
+                <input type="email" name="comment_email" class="form-control" name="comment_email" placeholder="<?php echo $_SESSION['email'] ?>">
               </div>
 
               <div class="form-group">
