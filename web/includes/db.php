67556789c406f0b7bcdfa6126f826b6c6942db1d
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2023-08-21 10:53:18 -0400

    Changing db.php logic

--- web/includes/db.php
+++ web/includes/db.php
@@ -23,9 +23,9 @@
   }
 
   // $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-  $conn = new mysqli($db['db_host'], $db['db_user'], $db['db_pass'], $db['db_name']);
+  $connection = new mysqli($db['db_host'], $db['db_user'], $db['db_pass'], $db['db_name']);
 
   $query = "SET NAMES utf8";
 
-  mysqli_query($conn, $query);
+  mysqli_query($connection, $query);
 ?>
