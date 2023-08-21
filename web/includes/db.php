ecdcc82c0179224ad9e15785de2e34c5977aa114
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2023-08-21 11:25:00 -0400

    Incorporating PDO for db connection

--- web/includes/db.php
+++ web/includes/db.php
@@ -21,11 +21,26 @@
   foreach($db as $key => $value){
     define(strtoupper($key), $value);
   }
+// mysql://be41329e6dea79:3cc4a440@us-cdbr-east-06.cleardb.net/heroku_981847eb2ca20f2?reconnect=true
+
+  $servername = $db['db_host'];
+  $database = $db['db_name'];
+  $username = $db['db_user'];
+  $password = $db['db_pass'];
+  $sql = "mysql:host=$servername;dbname=$database;";
+  $dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+  try {
+    $my_Db_Connection = new PDO($sql, $username, $password, $dsn_Options);
+    echo "Connected successfully";
+   } catch (PDOException $error) {
+    echo 'Connection error: ' . $error->getMessage();
+   }
 
   // $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-  $connection = new mysqli($db['db_host'], $db['db_user'], $db['db_pass'], $db['db_name']);
+  // $connection = new mysqli($db['db_host'], $db['db_user'], $db['db_pass'], $db['db_name']);
 
   $query = "SET NAMES utf8";
 
-  mysqli_query($connection, $query);
+  mysqli_query($my_Db_Connection, $query);
 ?>
