5dde5f9e5d6c7d1faf9bf0d803153bf4e87019d9
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2025-01-23 12:49:33 -0500

    Updated copyright year

--- web/includes/footer.php
+++ web/includes/footer.php
@@ -3,7 +3,7 @@
     <footer>
       <div class="row">
         <div class="col-lg-12 text-center">
-          <p class="footer-left">Copyright &copy; 2024 RockAltar </p>|<p class="footer-right"><a href="https://atkelley.github.io/" target="_blank">AK-47</a></p>
+          <p class="footer-left">Copyright &copy; 2025 RockAltar </p>|<p class="footer-right"><a href="https://atkelley.github.io/" target="_blank">AK-47</a></p>
         </div>
       </div>
     </footer>
