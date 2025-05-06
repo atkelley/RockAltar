09b4fd7b9de8bac57445b162997e5168311be824
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2025-05-06 13:23:28 -0400

    Using  instead of

--- web/includes/db.php
+++ web/includes/db.php
@@ -11,15 +11,12 @@
   $user = getenv('MYSQL_USER');
   $password = getenv('MYSQL_PASSWORD');
   $dbname = getenv('MYSQL_DATABASE');
-
-  try {
-      $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      echo "Connected successfully";
-  } catch(PDOException $e) {
-      echo "Connection failed: " . $e->getMessage();
+  
+  $connection = mysqli_connect($host, $user, $password, $dbname);
+  
+  if (!$connection) {
+      die("Database connection failed: " . mysqli_connect_error());
   }
-
   // if (getenv("MYSQL_HOST")) {
   //   $url = parse_url(getenv("JAWSDB_URL"));
 
