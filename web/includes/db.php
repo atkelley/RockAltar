9dcc2b1eb9d1762890ca9594f2384cb3ac58ec2d
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2025-05-06 13:04:08 -0400

    Some further corrections

--- web/includes/db.php
+++ web/includes/db.php
@@ -1,37 +1,23 @@
 <?php ob_start();
 
+  $db['db_host'] = "localhost";
+  $db['db_user'] = "root";
+  $db['db_pass'] = "";
+  $db['db_name'] = "rockAltar";
 
-$servername = getenv('MYSQL_HOST'); 
-$username = getenv('MYSQL_USER');
-$password = getenv('MYSQL_PASSWORD');
-$dbname = getenv('MYSQL_DATABASE');
+  if (getenv("MYSQL_HOST")) {
+    $db['db_host'] = getenv('MYSQL_HOST');
+    $db['db_user'] = getenv('MYSQL_USER');
+    $db['db_pass'] = getenv('MYSQL_PASSWORD');
+    $db['db_name'] = getenv('MYSQL_DATABASE');
+  }
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+  foreach($db as $key => $value){
+    define(strtoupper($key), $value);
+  }
 
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-echo "Connected successfully";
-
-  // $db['db_host'] = "localhost";
-  // $db['db_user'] = "root";
-  // $db['db_pass'] = "";
-  // $db['db_name'] = "rockAltar";
-
-  // if (getenv("MYSQL_HOST")) {
-  //   $db['db_host'] = getenv('MYSQL_HOST');
-  //   $db['db_user'] = getenv('MYSQL_USER');
-  //   $db['db_pass'] = getenv('MYSQL_PASSWORD');
-  //   $db['db_name'] = getenv('MYSQL_DATABASE');
-  // }
-
-  // foreach($db as $key => $value){
-  //   define(strtoupper($key), $value);
-  // }
-
-  // $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+  $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
   $query = "SET NAMES utf8";
-  mysqli_query($conn, $query);
+  mysqli_query($connection, $query);
 ?>
