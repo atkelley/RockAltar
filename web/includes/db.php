8693069dbb28a84dd218b99057a08dd8ebdcc6c6
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2025-05-06 13:17:55 -0400

    Trying new approach

--- web/includes/db.php
+++ web/includes/db.php
@@ -1,23 +1,48 @@
 <?php ob_start();
 
-  $db['db_host'] = "localhost";
-  $db['db_user'] = "root";
-  $db['db_pass'] = "";
-  $db['db_name'] = "rockAltar";
+  // $db['db_host'] = "localhost";
+  // $db['db_user'] = "root";
+  // $db['db_pass'] = "";
+  // $db['db_name'] = "rockAltar";
 
-  if (getenv("MYSQL_HOST")) {
-    $db['db_host'] = getenv('MYSQL_HOST');
-    $db['db_user'] = getenv('MYSQL_USER');
-    $db['db_pass'] = getenv('MYSQL_PASSWORD');
-    $db['db_name'] = getenv('MYSQL_DATABASE');
+
+
+  $host = getenv('MYSQL_HOST');
+  $user = getenv('MYSQL_USER');
+  $password = getenv('MYSQL_PASSWORD');
+  $dbname = getenv('MYSQL_DATABASE');
+
+  try {
+      $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "Connected successfully";
+  } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
   }
 
-  foreach($db as $key => $value){
-    define(strtoupper($key), $value);
-  }
+  // if (getenv("MYSQL_HOST")) {
+  //   $url = parse_url(getenv("JAWSDB_URL"));
 
-  $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+  //   if ($url) {
+  //     $db['db_host'] = getenv('MYSQL_HOST');
+  //     $db['db_user'] = getenv('MYSQL_USER');
+  //     $db['db_pass'] = getenv('MYSQL_PASSWORD');
+  //     $db['db_name'] = getenv('MYSQL_DATABASE');
 
-  $query = "SET NAMES utf8";
-  mysqli_query($connection, $query);
+  //     else {
+  //     die("Unable to connect to site.");
+  //   }
+  //   } else {
+  //     die("Unable to connect to site.");
+  //   }
+  // }
+
+  // foreach($db as $key => $value){
+  //   define(strtoupper($key), $value);
+  // }
+
+  // $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+  // $query = "SET NAMES utf8";
+  // mysqli_query($connection, $query);
 ?>
